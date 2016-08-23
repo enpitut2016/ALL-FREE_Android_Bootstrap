2fdb32e7b63b6bb22c42b885bd7697149133e489
edanao <edaeda.nao@gmail.com>
2016-08-23 17:27:15 +0900

    「前も言ったよ！」

--- app/src/main/java/com/example/allfree/conotan_bootstrap/HelpDetailActivity.java
+++ app/src/main/java/com/example/allfree/conotan_bootstrap/HelpDetailActivity.java
@@ -1,11 +1,14 @@
 package com.example.allfree.conotan_bootstrap;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.beardedhen.androidbootstrap.BootstrapButton;
 
 public class HelpDetailActivity extends AppCompatActivity {
 
@@ -15,6 +18,15 @@ public class HelpDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_help_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        BootstrapButton parent_button = (BootstrapButton)findViewById(R.id.button_maemoittayo);
+        parent_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HelpDetailActivity.this, RememberActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
 
