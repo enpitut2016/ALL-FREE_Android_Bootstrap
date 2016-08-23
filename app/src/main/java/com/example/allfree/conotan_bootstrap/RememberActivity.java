202b57df24e5b05f57e9901cd2e012fe0beb5a84
ferretdayo <ferretdayo@hotmail.co.jp>
2016-08-23 18:55:40 +0900

    fontsizeを全体的に大きくした

--- app/src/main/java/com/example/allfree/conotan_bootstrap/RememberActivity.java
+++ app/src/main/java/com/example/allfree/conotan_bootstrap/RememberActivity.java
@@ -2,6 +2,9 @@ package com.example.allfree.conotan_bootstrap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class RememberActivity extends AppCompatActivity {
 
@@ -9,7 +12,8 @@ public class RememberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remember);
-
+        TextView tv = (TextView)findViewById(R.id.text);
+        tv.setTextSize(70);
 
     }
 }
