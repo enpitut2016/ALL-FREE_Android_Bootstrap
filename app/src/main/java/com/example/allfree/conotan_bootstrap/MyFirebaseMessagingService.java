9c9dc04a50043bc264e2231f3c0f51f2152bee51
mattun-mart <m2mugtst3571@gmail.com>
2016-08-27 23:38:23 +0900

    FCMを用いたpush通知実装

---
+++ app/src/main/java/com/example/allfree/conotan_bootstrap/MyFirebaseMessagingService.java
@@ -0,0 +1,30 @@
+package com.example.allfree.conotan_bootstrap;
+
+import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by MatsudaAtsuto on 2016/08/27.
+ */
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final int NOTIFICATION_ID = 0;
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        String title = remoteMessage.getNotification().getTitle();
+        String body = remoteMessage.getNotification().getBody();
+
+        NotificationCompat.Builder builder =
+                (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle(title)
+                        .setContentText(body);
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.notify(NOTIFICATION_ID, builder.build());
+    }
+}
