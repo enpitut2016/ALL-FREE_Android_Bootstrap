281d0b6b6c518450bd318f38505cc5bfa52686f8
ferretdayo <ferretdayo@hotmail.co.jp>
2016-08-29 14:38:20 +0900

    テストコード１つ動くものかけた！

---
+++ app/src/androidTest/java/com/example/allfree/conotan_bootstrap/MainActivityTest.java
@@ -0,0 +1,47 @@
+package com.example.allfree.conotan_bootstrap;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by tsuruda_tomohiro on 2016/08/29.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private MainActivity mActivity;
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        mActivity = getActivity();
+    }
+    @Test
+    public void testCounterShow() {
+        onView(withId(R.id.parent)).perform(click());
+        onView(withId(R.id.send_text)).perform(typeText("testhoge"));
+        onView(withId(R.id.send)).perform(click());
+        onView(withId(R.id.help_text)).check(matches(withText("testhoge")));
+    }
+}
