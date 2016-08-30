6a448d67d69e2e744695b75e0d83c69717e857c1
ferretdayo <ferretdayo@hotmail.co.jp>
2016-08-30 16:37:46 +0900

    push通知を押すと対象のActivityに移動

--- app/src/main/java/com/example/allfree/conotan_bootstrap/ManualImageActivity.java
+++ app/src/main/java/com/example/allfree/conotan_bootstrap/ManualImageActivity.java
@@ -1,10 +1,12 @@
 package com.example.allfree.conotan_bootstrap;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class ManualImageActivity extends AppCompatActivity {
@@ -13,7 +15,11 @@ public class ManualImageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_image);
-        
+
+        //前のIntentから選択されたマニュアルのタイトルを取得
+        Intent intent = getIntent();
+        String url = intent.getStringExtra("url");
+        Log.d("URL", url);
     }
 
 }
